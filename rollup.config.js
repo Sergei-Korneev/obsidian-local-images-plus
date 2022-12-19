6fce4e476125446167744d0108f3b1b8ca4b1448
Sergei Korneev <s.m.korneev@yandex.ru>
2022-12-19 12:55:44 +0500

    Some changes 160311518329756

--- rollup.config.js
+++ rollup.config.js
@@ -18,7 +18,7 @@ if you want to view the source visit the plugins github repository
 export default {
   input: "src/main.ts",
   output: {
-    dir: "obsidian-local-images-continued",
+    dir: "obsidian-local-images-plus",
     sourcemap: "inline",
     sourcemapExcludeSources: isProd,
     format: "cjs",
